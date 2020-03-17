4074cc2850990d07ba707e6a272dc33f9eb94d7d
Nazmi Altun <nazmialtun@windowslive.com>
2020-03-17 11:54:26 +0800

    missing slash added

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -3,7 +3,7 @@ docker pull nazmialtun/testokur-identity:latest
 docker stop testokur-identity && docker rm testokur-identity --force
 docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  -d \
 	--env-file /home/env/identity.env \
-	-v /home/cert:/app/cert
+	-v /home/cert:/app/cert \
 	--name testokur-identity \
 	--restart=unless-stopped \
 	--network=testokur \
